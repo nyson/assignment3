ee13dfade6a992e22d29fe869d7398997c9af149
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2013-03-22 13:06:18 +0100

    Added database connectivity

--- src/testDBUsage.java
+++ src/testDBUsage.java
@@ -5,11 +5,26 @@ public class testDBUsage {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
 		Persons pers = new Persons();
-		
+		Accounts accs = new Accounts();
 		try {
-			System.out.println(pers.getPersonByName("Sylvester"));
-			System.out.println(pers.getPersonByName("Yosemite Hitler"));
+			System.out.println("Fetches accounts...");
+			System.out.println(Account.toStringHeader());
+			for(Account a : accs.getAccounts()) {
+				System.out.println(a);
+			}
+			
+			System.out.println("\nFetches persons...");
+			System.out.println(Person.toStringHeader());
+			for(Person p : pers.getPersons()) {
+				System.out.println(p);
+			}
+			
+			if(pers.add("Harry", "Gramsestad", "Räfsstad 20", 12034))
+				System.out.println("Added harry!");
+			else
+				System.out.println("Failed to add harry!");
 
+			
 		} catch (NoSuchRowException e) {
 			System.out.println(e.getMessage());
 		
