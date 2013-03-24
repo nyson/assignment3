5af5fe2ffdc2fa40bf99d198fe49ac6508bd8d84
Magnus Duberg <gusdubma@student.gu.se>
2013-03-24 18:15:35 +0100

    Removed panel listeners from Start.java listeners

--- src/Start.java
+++ src/Start.java
@@ -7,9 +7,6 @@ public class Start extends JFrame {
 	// don't know, eclipse forced me hand
 	private static final long serialVersionUID = -8523424458968940482L;
 	
-	// The Bank object that handles the SQL database
-	Bank werasBank = new Bank();
-	
 	// Graphical elements to be used
 	//Menus
 	JMenuBar menuBar = new JMenuBar();
@@ -24,11 +21,7 @@ public class Start extends JFrame {
 		transferMenuItem = new JMenuItem("Överföring"),
 		statementMenuItem = new JMenuItem("Kontoutdrag"),
 		aboutMenuItem = new JMenuItem("Om");
-	
-	//Test code
-	JLabel testLabel = new JLabel("Testfield (lies in Start panel)");
-	JTextField testTextField = new JTextField(20);
-	
+		
 	//Panels
 	ConnectDBPanel connectDBPanel = new ConnectDBPanel();
 	PersonPanel personPanel = new PersonPanel();
@@ -41,7 +34,7 @@ public class Start extends JFrame {
 	WeraPanel activePanel = connectDBPanel; // the Connect to database panel
 	
 	/**
-	 *  Listener for all buttons (and menus which are buttons)
+	 *  Listener for menus
 	 */
 	ActionListener startActionListener = new ActionListener() {
 		// Creates an inner class
@@ -49,8 +42,7 @@ public class Start extends JFrame {
 
 			activePanel.setVisible(false);	// hide the active panel
 
-			testTextField.setText(e.getSource().toString());
-			// select a new panel?
+			// select a new panel
 			if (e.getSource() == connectDBMenuItem)
 		    	 activePanel = connectDBPanel;
 			else if (e.getSource() == newPersonMenuItem)
@@ -64,54 +56,20 @@ public class Start extends JFrame {
 			else if (e.getSource() == statementMenuItem)
 				activePanel = statementPanel;
 			else if (e.getSource() == aboutMenuItem){
-				String aboutMessage = "Denna kan bytas sen till en hur" +
-						" fflasshig som helst.\n\n Skapad av:\n" +
-						" Jonatha\n Oskar\n Magnus";
 				JOptionPane.showMessageDialog(rootPane, aboutMessage, "Om" +
 						" Weras betalservice", JOptionPane.INFORMATION_MESSAGE);
 			}// end of if() statement			
 			activePanel.setVisible(true);	// show the selected panel
 		} // end of inner class
-	}; // End of Listener for all buttons
+	}; // End of Listener for menus
 	
-	/**
-	 * Listener for the "trigger" buttons in the panels
-	 */ 
- 	ActionListener panelActionListener = new ActionListener() {
- 		public void actionPerformed(ActionEvent e) {
- 			String queryString = "queryString not collected";
- 			
- 			if  (e.getSource() == connectDBPanel.addButton){
- 				
- 			}
- 			
- 			if  (e.getSource() == personPanel.addButton){
- 				Person p = new Person(personPanel.name, personPanel.street,
-							  personPanel.zip, personPanel.city);
- 				try{
- 					personPanel.handleAnswer(new Boolean(p.push()).toString());
- 				}catch(Exception exx) {}
- 			}
- 			if  (e.getSource() == accountPanel.addButton){
- 				
- 			}
- 			if  (e.getSource() == depositWithdrawPanel.performButton){
- 				
- 			}
- 			if  (e.getSource() == transferPanel.addButton){
- 				
- 			}
- 			if  (e.getSource() == statementPanel.showStatementButton){
- 				
- 			}
- 		 			
- 			//activePanel.handleAnswer(queryBank(queryString));
- 		} // end of inner class
- 	}; // end of Listener for add button in personPanel
-
+	
 	// Other class wide settings that can be reused
+	String aboutMessage = "Denna kan bytas sen till en hur" +
+			" fflasshig som helst.\n\n Skapad av:\n" +
+			" Jonatha\n Oskar\n Magnus";
 	Dimension windowSize = new Dimension(500, 350);
-	Dimension panelSize = new Dimension(300,100);
+	Dimension spacer =  new Dimension(450,20);
 	
 
 	/**
@@ -158,29 +116,15 @@ public class Start extends JFrame {
 		statementMenuItem.addActionListener(startActionListener);
 		aboutMenuItem.addActionListener(startActionListener);
 		
-		// Connect action listeners to the sub panels
-    	connectDBPanel.addButton.addActionListener(panelActionListener);
-    	personPanel.addButton.addActionListener(panelActionListener);
-    	accountPanel.addButton.addActionListener(panelActionListener);
-    	depositWithdrawPanel.performButton.addActionListener(panelActionListener);
-    	transferPanel.addButton.addActionListener(panelActionListener);
-    	statementPanel.showStatementButton.addActionListener(panelActionListener);
-				
-		// Add panels, buttons and so on..
+	// Add panels, buttons and so on..
 		// A spacer before the active panel
-		add(new Box.Filler(new Dimension(450,20), new Dimension(450,20),
-				new Dimension(200,20))); 
+		add(new Box.Filler(spacer, spacer, spacer)); 
 		// Add the panels
 		add(connectDBPanel);add(personPanel);add(accountPanel);
 		add(depositWithdrawPanel);add(transferPanel);add(statementPanel);
 		// A spacer after before the active panel
-		add(new Box.Filler(new Dimension(450,20), new Dimension(450,20),
-				new Dimension(450,20))); 
+		add(new Box.Filler(spacer, spacer, spacer)); 
 
-/*TEST CODE*/add(testLabel);add(testTextField); // Test field  
-
-		// we prefer NOT to pack(); here
-		// Show the active panel
 		activePanel.setVisible(true);  
 	}
 
@@ -196,17 +140,4 @@ public class Start extends JFrame {
 		e1.setVisible(true);
 		System.out.println("Weras betalservice is running...");
 	}
-	
-	/**
-	 * Sends a query to a Bank object and returns an answer String
-	 * @param query
-	 */
-	String queryBank(String query){
-		
-		String answer = werasBank.query(query);
-		return answer;
-	}
-	
-
-	
 }
